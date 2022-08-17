80d77aabfd384f92406a31caf1809ea865591f48
Gabriel Wu <wuzihua@pku.edu.cn>
2022-08-17 09:52:39 +0800

    refactor: inline short functions

--- src/special_functions.jl
+++ src/special_functions.jl
@@ -1,24 +1,22 @@
 """
 Calculate factorials using the Gamma function.
 """
-function factorial(T::DataType, n)
+@inline function factorial(T::DataType, n)
     if T <: Arblib.ArbLike
         return Arblib.gamma!(Arblib.Arb(), Arblib.Arb(n + 1))
     elseif T <: Arblib.AcbLike
         return Arblib.gamma!(Arblib.Acb(), Arblib.Acb(n + 1))
-    elseif T <: BigFloat
-        return SpecialFunctions.gamma(big(n + 1))
     else
-        return SpecialFunctions.gamma(n + 1)
+        return SpecialFunctions.gamma(T(n + 1))
     end
 end
 
-factorial(n) = factorial(Float64, n)
+@inline factorial(n) = factorial(Float64, n)
 
 """
 Spherical Bessel function of the first kind.
 """
-function spherical_jn(T::DataType, n::Integer, z)
+@inline function spherical_jn(T::DataType, n::Integer, z)
     if T <: Arblib.ArbLike
         return √(Arblib.Arb(π) / (2z)) * Arblib.hypgeom_bessel_j!(Arblib.Arb(), Arblib.Arb(n + 1 // 2), Arblib.Arb(z))
     elseif T <: Arblib.AcbLike
@@ -28,14 +26,14 @@ function spherical_jn(T::DataType, n::Integer, z)
     end
 end
 
-spherical_jn(n::Integer, z) = spherical_jn(Float64, n, z)
+@inline spherical_jn(n::Integer, z) = spherical_jn(Float64, n, z)
 
 """
 First-order derivative of spherical Bessel function of the first kind.
 """
-spherical_jn_deriv(T::DataType, n::Integer, z) = spherical_jn(T, n - 1, z) - (n + 1) / z * spherical_jn(T, n, z)
+@inline spherical_jn_deriv(T::DataType, n::Integer, z) = spherical_jn(T, n - 1, z) - (n + 1) / z * spherical_jn(T, n, z)
 
-spherical_jn_deriv(n::Integer, z) = spherical_jn_deriv(Float64, n, z)
+@inline spherical_jn_deriv(n::Integer, z) = spherical_jn_deriv(Float64, n, z)
 
 """
 Spherical Bessel function of the second kind.
@@ -50,108 +48,112 @@ function spherical_yn(T::DataType, n::Integer, z)
     end
 end
 
-spherical_yn(n::Integer, z) = spherical_yn(Float64, n, z)
+@inline spherical_yn(n::Integer, z) = spherical_yn(Float64, n, z)
 
 """
 First-order derivative of spherical Bessel function of the second kind.
 """
-spherical_yn_deriv(T::DataType, n::Integer, z) = spherical_yn(T, n - 1, z) - (n + 1) / z * spherical_yn(T, n, z)
+@inline spherical_yn_deriv(T::DataType, n::Integer, z) = spherical_yn(T, n - 1, z) - (n + 1) / z * spherical_yn(T, n, z)
 
-spherical_yn_deriv(n::Integer, z) = spherical_yn_deriv(Float64, n, z)
+@inline spherical_yn_deriv(n::Integer, z) = spherical_yn_deriv(Float64, n, z)
 
 """
 Spherical Hankel function of the first kind.
 """
-spherical_hn1(T::DataType, n::Integer, z) = spherical_jn(T, n, z) + 1im * spherical_yn(T, n, z)
+@inline spherical_hn1(T::DataType, n::Integer, z) = spherical_jn(T, n, z) + 1im * spherical_yn(T, n, z)
 
-spherical_hn1(n::Integer, z) = spherical_hn1(Float64, n, z)
+@inline spherical_hn1(n::Integer, z) = spherical_hn1(Float64, n, z)
 
 """
 First-order derivative of spherical Hankel function of the first kind.
 """
-spherical_hn1_deriv(T::DataType, n::Integer, z) = spherical_jn_deriv(T, n, z) + 1im * spherical_yn_deriv(T, n, z)
+@inline function spherical_hn1_deriv(T::DataType, n::Integer, z)
+    return spherical_jn_deriv(T, n, z) + 1im * spherical_yn_deriv(T, n, z)
+end
 
-spherical_hn1_deriv(n::Integer, z) = spherical_hn1_deriv(Float64, n, z)
+@inline spherical_hn1_deriv(n::Integer, z) = spherical_hn1_deriv(Float64, n, z)
 
 """
 Spherical Hankel function of the second kind.
 """
-spherical_hn2(T::DataType, n::Integer, z) = spherical_jn(T, n, z) - 1im * spherical_yn(T, n, z)
+@inline spherical_hn2(T::DataType, n::Integer, z) = spherical_jn(T, n, z) - 1im * spherical_yn(T, n, z)
 
-spherical_hn2(n::Integer, z) = spherical_hn2(Float64, n, z)
+@inline spherical_hn2(n::Integer, z) = spherical_hn2(Float64, n, z)
 
 """
 First-order derivative of spherical Hankel function of the second kind.
 """
-spherical_hn2_deriv(T::DataType, n::Integer, z) = spherical_jn_deriv(T, n, z) - 1im * spherical_yn_deriv(T, n, z)
+@inline function spherical_hn2_deriv(T::DataType, n::Integer, z)
+    return spherical_jn_deriv(T, n, z) - 1im * spherical_yn_deriv(T, n, z)
+end
 
-spherical_hn2_deriv(n::Integer, z) = spherical_hn2_deriv(Float64, n, z)
+@inline spherical_hn2_deriv(n::Integer, z) = spherical_hn2_deriv(Float64, n, z)
 
 """
 Riccati Bessel function of the first kind.
 """
-ricatti_jn(T::DataType, n::Integer, z) = z * spherical_jn(T, n, z)
+@inline ricatti_jn(T::DataType, n::Integer, z) = z * spherical_jn(T, n, z)
 
-ricatti_jn(n::Integer, z) = ricatti_jn(Float64, n, z)
+@inline ricatti_jn(n::Integer, z) = ricatti_jn(Float64, n, z)
 
 """
 First-order derivative of Riccati Bessel function of the first kind.
 """
-function ricatti_jn_deriv(T::DataType, n::Integer, z)
+@inline function ricatti_jn_deriv(T::DataType, n::Integer, z)
     jn = spherical_jn(T, n, z)
     jnd = spherical_jn_deriv(T, n, z)
     return z * jnd + jn
 end
 
-ricatti_jn_deriv(n::Integer, z) = ricatti_jn_deriv(Float64, n, z)
+@inline ricatti_jn_deriv(n::Integer, z) = ricatti_jn_deriv(Float64, n, z)
 
 """
 Riccati Bessel function of the second kind.
 
 > Note that in `miepy`, the author used `-z⋅y(z)` instead of `z⋅y(z)`
 """
-ricatti_yn(T::DataType, n::Integer, z) = z * spherical_yn(T, n, z)
+@inline ricatti_yn(T::DataType, n::Integer, z) = z * spherical_yn(T, n, z)
 
-ricatti_yn(n::Integer, z) = ricatti_yn(Float64, n, z)
+@inline ricatti_yn(n::Integer, z) = ricatti_yn(Float64, n, z)
 
 """
 First-order derivative of Riccati Bessel function of the second kind.
 """
-function ricatti_yn_deriv(T::DataType, n::Integer, z)
+@inline function ricatti_yn_deriv(T::DataType, n::Integer, z)
     yn = spherical_yn(T, n, z)
     ynd = spherical_yn_deriv(T, n, z)
     return z * ynd + yn
 end
 
-ricatti_yn_deriv(n::Integer, z) = ricatti_yn_deriv(Float64, n, z)
+@inline ricatti_yn_deriv(n::Integer, z) = ricatti_yn_deriv(Float64, n, z)
 
 """
 Riccati Hankel function of the first kind.
 """
-ricatti_hn1(T::DataType, n::Integer, z) = ricatti_jn(T, n, z) + 1im * ricatti_yn(T, n, z)
+@inline ricatti_hn1(T::DataType, n::Integer, z) = ricatti_jn(T, n, z) + 1im * ricatti_yn(T, n, z)
 
-ricatti_hn1(n::Integer, z) = ricatti_hn1(Float64, n, z)
+@inline ricatti_hn1(n::Integer, z) = ricatti_hn1(Float64, n, z)
 
 """
 First-order derivative of Riccati Hankel function of the first kind.
 """
-ricatti_hn1_deriv(T::DataType, n::Integer, z) = ricatti_jn_deriv(T, n, z) + 1im * ricatti_yn_deriv(T, n, z)
+@inline ricatti_hn1_deriv(T::DataType, n::Integer, z) = ricatti_jn_deriv(T, n, z) + 1im * ricatti_yn_deriv(T, n, z)
 
-ricatti_hn1_deriv(n::Integer, z) = ricatti_hn1_deriv(Float64, n, z)
+@inline ricatti_hn1_deriv(n::Integer, z) = ricatti_hn1_deriv(Float64, n, z)
 
 """
 Riccati Hankel function of the second kind.
 """
-ricatti_hn2(T::DataType, n::Integer, z) = ricatti_jn(T, n, z) - 1im * ricatti_yn(T, n, z)
+@inline ricatti_hn2(T::DataType, n::Integer, z) = ricatti_jn(T, n, z) - 1im * ricatti_yn(T, n, z)
 
-ricatti_hn2(n::Integer, z) = ricatti_hn2(Float64, n, z)
+@inline ricatti_hn2(n::Integer, z) = ricatti_hn2(Float64, n, z)
 
 """
 First-order derivative of Riccati Hankel function of the second kind.
 """
-ricatti_hn2_deriv(T::DataType, n::Integer, z) = ricatti_jn_deriv(T, n, z) - 1im * ricatti_yn_deriv(T, n, z)
+@inline ricatti_hn2_deriv(T::DataType, n::Integer, z) = ricatti_jn_deriv(T, n, z) - 1im * ricatti_yn_deriv(T, n, z)
 
-ricatti_hn2_deriv(n::Integer, z) = ricatti_hn2_deriv(Float64, n, z)
+@inline ricatti_hn2_deriv(n::Integer, z) = ricatti_hn2_deriv(Float64, n, z)
 
 @doc raw"""
 Associated Legendre function without the Condon-Shotley phase.
@@ -173,7 +175,7 @@ function associated_legendre(T::DataType, n::Integer, m::Integer, z)
     end
 end
 
-associated_legendre(n::Integer, m::Integer, z) = associated_legendre(Float64, n, m, z)
+@inline associated_legendre(n::Integer, m::Integer, z) = associated_legendre(Float64, n, m, z)
 
 @doc raw"""
 Calculate the associated Legendre function for all ``0\leq n\leq n_{\max}`` and ``-n\leq m\leq n``, and return the results as a vector.
@@ -223,7 +225,7 @@ function associated_legendre_array(T::DataType, nmax::Integer, z)
     return ret
 end
 
-associated_legendre_array(nmax::Integer, z) = associated_legendre_array(Float64, nmax, z)
+@inline associated_legendre_array(nmax::Integer, z) = associated_legendre_array(Float64, nmax, z)
 
 @doc raw"""
 Calculate ``\pi_n^m(\theta)`` defined as
@@ -256,7 +258,7 @@ function pi_func(T::DataType, n::Integer, m::Integer, θ::Number)
     end
 end
 
-pi_func(n::Integer, m::Integer, θ::Number) = pi_func(Float64, n, m, θ)
+@inline pi_func(n::Integer, m::Integer, θ::Number) = pi_func(Float64, n, m, θ)
 
 @doc raw"""
 Calculate ``\tau_n^m(\theta)`` defined as
@@ -305,21 +307,22 @@ function tau_func(T::DataType, n::Integer, m::Integer, θ::Number)
     end
 end
 
-tau_func(n::Integer, m::Integer, θ) = tau_func(Float64, n, m, θ)
+@inline tau_func(n::Integer, m::Integer, θ) = tau_func(Float64, n, m, θ)
 
 """
 Wigner 3-j symbols.
 
 """
-function wigner_3j(T::DataType, j1::Integer, j2::Integer, j3::Integer, m1::Integer, m2::Integer, m3::Integer=-m1 - m2)
+@inline function wigner_3j(T::DataType, j1::Integer, j2::Integer, j3::Integer, m1::Integer, m2::Integer,
+                           m3::Integer=-m1 - m2)
     if T <: Union{Arblib.ArbLike,BigFloat}
         return WignerSymbols.wigner3j(T, j1, j2, j3, m1, m2, m3)
     else
-        return GSL.sf_coupling_3j(2j1, 2j2, 2j3, 2m1, 2m2, 2m3)
+        return T(GSL.sf_coupling_3j(2j1, 2j2, 2j3, 2m1, 2m2, 2m3))
     end
 end
 
-function wigner_3j(j1::Integer, j2::Integer, j3::Integer, m1::Integer, m2::Integer, m3::Integer=-m1 - m2)
+@inline function wigner_3j(j1::Integer, j2::Integer, j3::Integer, m1::Integer, m2::Integer, m3::Integer=-m1 - m2)
     return wigner_3j(Float64, j1, j2, j3, m1, m2, m3)
 end
 
@@ -335,7 +338,7 @@ function a_func(T::DataType, n::Integer, v::Integer, p::Integer, m::Integer, u::
     return factor * w1 * w2
 end
 
-a_func(n::Integer, v::Integer, p::Integer, m::Integer, u::Integer) = a_func(Float64, n, v, p, m, u)
+@inline a_func(n::Integer, v::Integer, p::Integer, m::Integer, u::Integer) = a_func(Float64, n, v, p, m, u)
 
 """
 b-function.
@@ -349,4 +352,4 @@ function b_func(T::DataType, n::Integer, v::Integer, p::Integer, m::Integer, u::
     return factor * w1 * w2
 end
 
-b_func(n::Integer, v::Integer, p::Integer, m::Integer, u::Integer) = b_func(Float64, n, v, p, m, u)
+@inline b_func(n::Integer, v::Integer, p::Integer, m::Integer, u::Integer) = b_func(Float64, n, v, p, m, u)
